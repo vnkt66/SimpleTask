a1b9eb3c84888831546f1f683dc4e610a1840556
venkat <penubothuvenkannachowdary@gmail.com>
2019-12-13 18:50:21 +0530

    added times and search path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
       path = require('path'),
       cool = require('cool-ascii-faces'),
+      fs = require('fs'),
       PORT = process.env.PORT || 5000
 
   var app = express();
@@ -12,11 +13,33 @@ const express = require('express'),
   app.set('view engine', 'ejs');
 
   app.get('/', (req, res) => {
-    res.render('pages/index');
+    // res.render('pages/index');
+    res.send('Hello, Navigate through the paths to see result');
   })
 
   app.get('/cool', (req, res) => {
     res.send(cool());
+  });
+
+  app.get('/times', (req, res) => {
+    res.send(showTimes());
   })
+
+  app.get('/search', (req, res) => {
+    let id = req.query.id;
+    let data = fs.readFileSync('search.json');
+    let jsondata = JSON.parse(data);
+    let dataarray = [...jsondata.users];
+    let filtered = dataarray.filter((obj) => obj.id === +id);
+
+    res.send(filtered[0].name);
+  })
+
+  const showTimes = () => {
+     let res = ''; 
+     const time = process.env.Times || 3;
+     res = time * time; 
+     return res;
+  } 
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
