c26f82b25f3b3d1923c2f1bdddcf193e37cf81d7
venkat <penubothuvenkannachowdary@gmail.com>
2019-12-13 18:05:17 +0530

    added cool package

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express'),
+      path = require('path'),
+      cool = require('cool-ascii-faces'),
+      PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  var app = express();
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.set('views', path.join(__dirname, 'views'));
+
+  app.set('view engine', 'ejs');
+
+  app.get('/', (req, res) => {
+    res.render('pages/index');
+  })
+
+  app.get('/cool', (req, res) => {
+    res.send(cool());
+  })
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
